e650f7d44d3850ac287d7e4184483251ee47897f
AldaraMG~ <almaga71@gmail.com>
2022-12-21 20:39:47 +0100

    musica y gift

--- js/jugador azar.js
+++ js/jugador azar.js
@@ -1,54 +1,54 @@
-window.onload = function() {
+window.onload = function () {
     window.datos = [];
 }
 
 function agregarElemento() {
-    let listaItems = document.getElementById('Nombre_input').value;
+    let nuevoJugador = document.getElementById('Nombre_input').value;
 
-    datos.push(listaItems);
-    
-    
+    datos.push(nuevoJugador);
+    mostrarArreglo();
+
+
 }
 
 function mostrarArreglo() {
     let resultado = document.getElementById('resultado');
     resultado.innerHTML = '';
 
-    for (const listaItems of datos) {
+    for (const nuevoJugador of datos) {
         let datoParrafo = document.createElement('p');
-        datoParrafo.innerText = listaItems;
+        datoParrafo.innerText = nuevoJugador;
 
         resultado.appendChild(datoParrafo);
     }
 }
 
-function eliminarElemento () {
-    let listaItems = document.getElementById('Nombre_input').value;
-
-    datos.pop(listaItems);
+function eliminarElemento() {
+    let nuevoJugador = document.getElementById('Nombre_input').value;
+    datos.pop();
+    mostrarArreglo();
 
 }
 
-function jugarElemento (){
+function jugarElemento() {
     cambiar();
-    let listaItems = document.getElementById('Nombre_input').value;
+    let nuevoJugador = document.getElementById('Nombre_input').value;
+    let Azar = document.getElementById('azar-nombres'); Azar.innerHTML = datos[Math.floor(Math.random() * datos.length)];
 
-    let Azar = document.getElementById('azar-nombres');Azar.innerHTML = datos[Math.floor(Math.random() * datos.length  )];
-    
-   
 
-    for (const listaItems of datos) {
+
+    for (const nuevoJugador of datos) {
         let datoParrafo = document.createElement('p');
         datoParrafo.innerText = listaItems;
 
         listaItems.innerText(datoParrafo);
     }
-    
 
-   
+
+
 }
 
-     
+
 
 
 
